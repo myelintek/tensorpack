beae4d5aaecdec6e2763a6496766c66d65dd1e0a
gt758215 <gt758215@users.noreply.github.com>
2018-02-04 01:36:54 +0800

    Update training.py

--- tensorpack/graph_builder/training.py
+++ tensorpack/graph_builder/training.py
@@ -190,23 +190,28 @@ class SyncMultiGPUReplicatedBuilder(DataParallelBuilder):
             use_vs=[False] + [True] * (len(self.towers) - 1))
 
         DataParallelBuilder._check_grad_list(grad_list)
+        
+        grad_ops = []
+        for gvs in zip(*grad_list):
+            for g, _ in gvs:
+                grad_ops.append(g)
+
         opt = get_opt_fn()
         grads = allreduce_grads(grad_list)
         #grads = allreduce_grads_v2(grad_list, opt=opt)
 
         train_ops = []
-        accum_ops = []
         for idx, grad_and_vars in enumerate(grads):
             with tf.device(raw_devices[idx]):
                 # apply_gradients may create variables. Make them LOCAL_VARIABLES
                 with override_to_local_variable(enable=idx > 0):
                     train_ops.append(opt.apply_gradients(
                         grad_and_vars, name='apply_grad_{}'.format(idx)))
-                    accum_ops.append(opt._accum_grads)
+
         train_op = tf.group(*train_ops, name='train_op')
-        accum_op = tf.group(*accum_ops, name='accum_op')
+        accum_op = tf.group(*grad_ops, name='accum_op')
         post_init_op = SyncMultiGPUReplicatedBuilder.get_post_init_ops()
-        return train_op, post_init_op, accum_op, opt._counter, opt._niter
+        return train_op, post_init_op, accum_op
 
 # Adopt from https://github.com/tensorflow/benchmarks/blob/master/scripts/tf_cnn_benchmarks/variable_mgr.py
     @staticmethod
